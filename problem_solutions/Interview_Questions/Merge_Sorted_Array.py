e1928b7a445fc812bd4778f8f0302f8b3fb5c511
Alif-7040 <139248868+Alif-7040@users.noreply.github.com>
2023-08-12 15:05:58 -0400

    Update Merge_Sorted_Array.py

--- problem_solutions/Interview_Questions/Merge_Sorted_Array.py
+++ problem_solutions/Interview_Questions/Merge_Sorted_Array.py
@@ -1,43 +1,33 @@
 class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> 
-None:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Date: 8/12/2023
-        Time: 16:00
-        Purpose: Merges two different sorted arrays, of not necessarily the same size
+        Time: 16:04
+        Purpose: Merge sorted arrays
+        Runtime: 44 ms, beats 83.3% (Much better than the beats 10% I got with the other problems)
+        Memory: 16.39 mb, beats 56.31% (Same thing with runtime, much better than before)
+        Do not return anything, modify nums1 in-place instead.
         """
         i = 0 # pointer for nums1
         j = 0 # pointer for nums2
         loc_last_n1 = m - 1
         while True:
-            print(nums1)
-            print(i)
-            print(j)
             if j == n: # nothing else to insert
-                print("a")
                 return nums1
             if i > loc_last_n1: # put the rest of nums2 in nums1
-                print("b")
                 nums1[m+j : m+n] = nums2[j : n]
                 return nums1
             if nums1[i] < nums2[j]:
-                print("c")
                 i += 1
             elif nums1[i] == nums2[j]:
-                print("d")
                 nums1.insert(i, nums2[j])
                 nums1.pop()
                 loc_last_n1 += 1
                 i += 2
                 j += 1
             else: # nums[1] must be > nums2[j]
-                print("e")
                 nums1.insert(i, nums2[j])
                 nums1.pop()
                 loc_last_n1 += 1
                 i += 1
                 j += 1
-            print(nums1)
-            print(i)
-            print(j)
-        print("f")
